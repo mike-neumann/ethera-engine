a65b00cc4b926dfd6fde8138de5b023bf992b4e2
xRa1ny <xra1ny@icloud.com>
2024-10-20 10:10:28 +0200

    Refined sound system
    - Multiple sounds may now play simultaneously

--- src/main/kotlin/me/etheraengine/service/SoundService.kt
+++ src/main/kotlin/me/etheraengine/service/SoundService.kt
@@ -1,20 +1,20 @@
 package me.etheraengine.service
 
 import jakarta.annotation.PostConstruct
-import me.etheraengine.Sound
 import me.etheraengine.config.EtheraConfig
 import me.etheraengine.logger
 import org.springframework.stereotype.Service
 import org.springframework.util.ResourceUtils
 import java.io.File
 import java.io.FileFilter
+import javax.sound.sampled.AudioSystem
 
 @Service
 class SoundService(
     private val etheraConfig: EtheraConfig
 ) {
     private val log = logger<SoundService>()
-    private val sounds = mutableMapOf<String, Sound>()
+    private val sounds = mutableMapOf<String, File>()
 
     @PostConstruct
     fun init() {
@@ -31,14 +31,19 @@ class SoundService(
     fun getSound(key: String) =
         sounds.computeIfAbsent(key) {
             log.info("Loading sound {}", key)
-            Sound(ResourceUtils.getFile("${etheraConfig.soundUrl}/$key"))
+            ResourceUtils.getFile("${etheraConfig.soundUrl}/$key")
         }
 
-    fun stopSound(key: String) {
-        getSound(key).stop()
-    }
-
     fun playSound(key: String) {
-        getSound(key).start()
+        val clip = AudioSystem.getClip()
+
+        clip.open(AudioSystem.getAudioInputStream(getSound(key)))
+        clip.addLineListener {
+            if (clip.framePosition == clip.frameLength) {
+                clip.drain()
+                clip.stop()
+            }
+        }
+        clip.start()
     }
 }
